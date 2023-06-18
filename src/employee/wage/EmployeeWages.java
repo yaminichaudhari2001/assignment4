172a15d8167192edc157d8eb328a803a45538c31
yami <yaminichaudhari2001@gmail.com>
2023-06-18 22:44:47 +0530

    display in UC3

--- src/employee/wage/EmployeeWages.java
+++ src/employee/wage/EmployeeWages.java
@@ -2,12 +2,19 @@ package employee.wage;
 
 public class EmployeeWages {
     public static void main(String[] args) {
-        int IS_FFULLTIME=1;
+
+        int IS_PART_TIME=1;
+        int IS_FULL_TIME=2;
         int EMP_RATE_PER_HOUR=20;
         int empHrs=0;
         int empWage=0;
         Double empcheck=Math.floor(Math.random()*10)%2;
-            if(empcheck==IS_FFULLTIME)
+           if(empcheck==IS_PART_TIME)
+           {
+               empHrs=4;
+
+           }
+           else if(empcheck==IS_FULL_TIME)
             {
             empHrs=8;
             }
