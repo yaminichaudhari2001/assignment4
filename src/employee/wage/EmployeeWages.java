10c8ef99919094105a7cf1afaa678ac6809bf42e
yami <yaminichaudhari2001@gmail.com>
2023-06-18 23:07:02 +0530

    display in UC5

--- src/employee/wage/EmployeeWages.java
+++ src/employee/wage/EmployeeWages.java
@@ -1,30 +1,42 @@
+//Calculating Wages for
+//a Month assuming 20
+//Working Days in a
+//Month
 package employee.wage;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
 
 public class EmployeeWages {
    public static final int IS_PART_TIME=1;
     public static final int IS_FULL_TIME=2;
     public static final int EMP_RATE_PER_HOUR=20;
+    public static final int NUM_OF_WORKING_DAYS=20;
     public static void main(String[] args) {
-        int empHrs=0;
-        int empWage=0;
-        int empcheck=(int)Math.floor(Math.random()*10)%3;
-        switch (empcheck)
-        {
-            case IS_PART_TIME:
-           {
-               empHrs=4;
-               break;
-           }
-            case IS_FULL_TIME:
-            {
-            empHrs=8;
-            break;
-            }
-            default:
-                empHrs=0;
+        int empHrs = 0;
+        int empWage = 0;
+        int totalempWage = 0;
+        int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            switch (empcheck) {
+                case IS_PART_TIME: {
+                    empHrs = 4;
+                    break;
+                }
+                case IS_FULL_TIME: {
+                    empHrs = 8;
+                    break;
+                }
+                default:
+                    empHrs = 0;
 
             }
-            empWage=empHrs*EMP_RATE_PER_HOUR;
-        System.out.println("EmpWage "+empWage);
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalempWage=+empWage;
+            }
+        System.out.println("EmpWage " + empWage);
+        System.out.println("totalWage "+totalempWage);
     }
+
 }
