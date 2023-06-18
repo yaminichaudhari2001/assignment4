8bbcc5deb883374ebdb62b2f6ffbbf256e17614f
yami <yaminichaudhari2001@gmail.com>
2023-06-18 21:43:17 +0530

    display Welcom massage on master

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//Start with Displaying
+//Welcome to Employee
+//Wage Computation
+//Program on Master Branch
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+}
