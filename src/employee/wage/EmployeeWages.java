9b536d08b9f77b3b0ee200d0797f4c4dadca265d
yami <yaminichaudhari2001@gmail.com>
2023-06-18 22:38:46 +0530

    display in UC2

--- src/employee/wage/EmployeeWages.java
+++ src/employee/wage/EmployeeWages.java
@@ -3,14 +3,19 @@ package employee.wage;
 public class EmployeeWages {
     public static void main(String[] args) {
         int IS_FFULLTIME=1;
+        int EMP_RATE_PER_HOUR=20;
+        int empHrs=0;
+        int empWage=0;
         Double empcheck=Math.floor(Math.random()*10)%2;
             if(empcheck==IS_FFULLTIME)
             {
-                System.out.println("Employee is present");
+            empHrs=8;
             }
             else{
-                System.out.println("Employee is absent");
+                empHrs=0;
 
             }
+            empWage=empHrs*EMP_RATE_PER_HOUR;
+        System.out.println("EmpWage "+empWage);
     }
 }
